81b614ff95081d0a1056078bf2cfab727711e49a
soyombo <yosomboo@gmail.com>
2020-06-11 14:40:35 -0700

    shoog shiddeg bolgoh

--- app.js
+++ app.js
@@ -8,12 +8,18 @@ var socres = [0, 0];
 var roundScore = 0;
 
 // Side of the Dice (1-6)
-var dice = Math.floor(Math.random() * 6) + 1;
-// program eh;ehed beltgeh ni
 
+// program ehlehed beltgeh ni
 document.getElementById("score-0").textContent = "0";
 document.getElementById("score-1").textContent = "0";
 document.getElementById("current-0").textContent = "0";
 document.getElementById("current-1").textContent = "0";
 
-document.querySelector(".dice").style.display = "none";
+var diceDom = document.querySelector(".dice");
+diceDom.style.display = "none";
+
+document.querySelector(".btn-roll").addEventListener("click", function () {
+  var diceNumber = Math.floor(Math.random() * 6) + 1;
+  diceDom.style.display = "block";
+  diceDom.src = "dice-" + diceNumber + ".png";
+});
